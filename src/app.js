92878635fa05e777f49e723d9f0758686034252e
AgusLozano99 <aguspro99@gmail.com>
2022-08-30 18:16:24 -0300

    Avances sprint 3

--- src/app.js
+++ src/app.js
@@ -1,22 +1,18 @@
-const { application } = require("express");
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static('./public'));
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"));
-  });
-  app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
-  });
-  app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
-  });
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+
+const mainRoutes = require("./routes/mainRoutes");
+app.use('/', mainRoutes);
+
   app.get("/productCart", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
   });
