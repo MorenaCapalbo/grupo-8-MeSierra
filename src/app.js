029c1bf3ab002aabd92ad37d3c3900eb0c19fa13
GLecherbauer <german.lecher2110@gmail.com>
2022-10-10 22:54:59 -0300

    Correji datos del users.json y actualize la ruta de Products/list

--- src/app.js
+++ src/app.js
@@ -13,8 +13,10 @@ app.set('views', __dirname + '/views')
 const mainRoutes = require("./routes/mainRoutes");
 app.use('/', mainRoutes);
 
+
 // Ruta de products por GET
 
 const productsRoutes = require("./routes/products.js");
 
-app.use('/products', productsRoutes);
+app.use('/products', productsRoutes); 
+
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,20 +1,20 @@
 const productsController =  {
     list: (req,res) =>{
-        return res.send("Listado de productos desde el controlador")
+        return res.render("productList", {});
     },
 
     create: (req,res) => {
-        return res.send("Formulario de creacion de producto")
+        return res.render("productCreate", {})
     },
 
     detail: (req,res) => {
 
-        return res.send("Detalle del producto " + req.params.idDetail)
+        return res.send("Detalle del producto " + req.params.id)
 },
 
     edit: (req,res) => {
 
-        return res.send ("Formulario de edicion de producto")
+        return res.send ("Formulario de edicion de producto " + req.params.id)
 }
 }
 
