e35fba023f82b147eced7a5df812cc76903b816f
GLecherbauer <german.lecher2110@gmail.com>
2022-10-12 20:27:26 -0300

    Actualizacion de carpetas

--- src/app.js
+++ src/app.js
@@ -2,21 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000");
-});
-
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 
-const mainRoutes = require("./routes/mainRoutes");
-app.use('/', mainRoutes);
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
 
 
 // Ruta de products por GET
 
-const productsRoutes = require("./routes/products.js");
+const productsRoutes = require("./routes/productsRoutes");
+const mainRoutes = require("./routes/mainRoutes");
 
+app.use('/', mainRoutes);
 app.use('/products', productsRoutes); 
 
