cfb6dbdbc381dba69a68f94aaed4b50748030c42
Gonzalo Andres Rossi <AD173819@slad.la.mcdcorp>
2022-11-30 20:13:03 -0300

    relaciones tabla

---
+++ src/database/models/categoria.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = 'Categoria' // esto deberÃ­a estar en singular
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        nombre_categoria: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+       
+    }
+
+    let config = {
+        
+        tableName: "categorias",
+        timestamps: false
+    }
+    const Categoria = sequelize.define(alias,cols,config)
+
+    Categoria.associate = function(models) {
+        Categoria.hasMany(models.Productos, {
+            as: "productos",
+            foreignKey: "categoria_id",
+        })
+
+    }
+
+    return Categoria
+};
---
+++ src/database/models/marca.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = 'Marca' // esto deberÃ­a estar en singular
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        nombre_marca: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+       
+    }
+
+    let config = {
+        
+        tableName: "marcas",
+        timestamps: false
+    }
+    const Marca = sequelize.define(alias,cols,config)
+
+    Marca.associate = function(models) {
+        Marca.hasMany(models.Productos, {
+            as: "productos",
+            foreignKey: "marca_id"
+        })
+
+    }
+
+    return Marca
+};
