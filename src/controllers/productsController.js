2df3cc750e9fe0f874855914115b92688fa648cb
AgusLozano99 <108700113+AgusLozano99@users.noreply.github.com>
2022-12-10 19:13:37 -0300

    trabajando con martin

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -17,7 +17,11 @@ const toThousand = (n) => n.toString().replace(/\B(?=(\d{3}) + (?!\d))/g, ".");
 const productsController = {
   index: (req, res) => {
     db.Producto.findAll({
-      include: [{ association: "Marcas" }, { association: "Categorias" }],
+      include: [
+        { association: "Marcas" },
+        { association: "Categorias" },
+        { association: "Imagenes" },
+      ],
     }).then(function (respuesta) {
       return res.render("products", {
         respuesta,
@@ -28,28 +32,26 @@ const productsController = {
 
   detail: (req, res) => {
     const idProduct = req.params.id;
-      db.Producto.findByPk(idProduct, {
-        include: [{ association: "Marcas" }, { association: "Categorias" }],
-      }).then(function (producto) {
-        return res.render("productDetail", {
-          producto,
-          toThousand,
-        });
+    db.Producto.findByPk(idProduct, {
+      include: [{ association: "Marcas" }, { association: "Categorias" }],
+    }).then(function (producto) {
+      return res.render("productDetail", {
+        producto,
+        toThousand,
       });
-
+    });
   },
 
   create: (req, res) => {
-    db.Marca.findAll()
-    .then((marcas)=> {
+    db.Marca.findAll().then((marcas) => {
       res.render("../views/productCreate", {
-        marcas
-      })
-    })
+        marcas,
+      });
+    });
   },
 
   store: async (req, res) => {
-    let imagesSelect = req.file ? req.file.filename : no-foto.jpg;
+    let imagesSelect = req.file ? req.file.filename : no - foto.jpg;
     let NuevoProducto = 1;
     await db.Producto.create({
       nombre_producto: req.body.name,
@@ -57,23 +59,32 @@ const productsController = {
       descuento: Number(req.body.discount),
       estado: req.body.estado,
       stock: Number(req.body.stock),
-      categoria_id:req.body.category,
+      categoria_id: req.body.category,
       descripcion: req.body.description,
-      marca_id: Number(req.body.marca)
+      marca_id: Number(req.body.marca),
     }).then((producto) => {
-      NuevoProducto = producto
-    })
-      db.Imagen.create({
-        nombre_archivo: imagesSelect,
-        producto_id: Number(NuevoProducto.id)
-      }).then((product) => {
+      NuevoProducto = producto;
+    });
+    db.Imagen.create({
+      nombre_archivo: imagesSelect,
+      producto_id: Number(NuevoProducto.id),
+    }).then((product) => {
       res.redirect("/products");
     });
   },
 
-  edit: (req, res) => {
-    const idProduct = req.params.id;
-    res.send("Modificar producto " + idProduct);
+  edit: async (req, res) => {
+    const productById = req.params.id;
+    const marcas = await db.Marca.findAll()
+   await db.Producto.findByPk(productById, {
+      include: [
+        { association: "Marcas" },
+        { association: "Categorias" },
+        { association: "Imagenes" },
+      ],
+    }).then((respuesta) => {
+      return res.render("productEdit", { respuesta,marcas, toThousand });
+    });
   },
 
   update: (req, res) => {
